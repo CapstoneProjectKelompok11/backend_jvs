958ccf18aa4022e882ab51cb1bfdcfc8ff095a60
Fel-1 <felixreinaldosiong@yahoo.co.id>
2022-06-02 17:56:58 +0700

    add jwt filter

--- src/main/java/com/capstone/booking/config/security/SecurityConfiguration.java
+++ src/main/java/com/capstone/booking/config/security/SecurityConfiguration.java
@@ -8,10 +8,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +32,12 @@ public class SecurityConfiguration {
         http.headers().frameOptions().disable();
         http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/api/**").permitAll();
+
+        //remove session
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        //filter jwt
+        http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
