3af117286c7c9a707dc94b5de818859aabc25ebb
Fel-1 <felixreinaldosiong@yahoo.co.id>
2022-07-04 18:43:33 +0700

    change token expiration

--- src/main/java/com/capstone/booking/domain/dto/RegisterResponse.java
+++ src/main/java/com/capstone/booking/domain/dto/RegisterResponse.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
